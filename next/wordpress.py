b57a86ccc94661d8ac1bf124a22a571c61efe0a5
skywind3000 <skywind3000@163.com>
2019-05-05 14:00:13 +0800

    working with media

--- next/wordpress.py
+++ next/wordpress.py
@@ -69,19 +69,73 @@ class WordPress (object):
             newpost.content = ''
         return newpost
 
-    def new_post (self, post = None):
+    def post_new (self, post = None):
         newpost = self.__convert_post(post)
         action = wordpress_xmlrpc.methods.posts.NewPost(newpost)
         pid = self._client.call(action)
         return pid
 
-    def edit_post (self, post):
+    def post_edit (self, post):
         if 'id' not in post:
             raise ValueError('missing id in post')
         newpost = self.__convert_post(post)
         pid = post['id']
         action = wordpress_xmlrpc.methods.posts.EditPost(pid, newpost)
         return self._client.call(action)
+
+    def post_get (self, pid):
+        action = wordpress_xmlrpc.methods.posts.GetPost(pid)
+        return self._client.call(action)
+
+    # keys of query: number, offset, orderby, order(ASC/DESC), post_type
+    # and post_status
+    # returns list of WordPressPost instances
+    def post_list (self, query):
+        action = wordpress_xmlrpc.methods.posts.GetPosts(query)
+        return self._client.call(action)
+
+    # returns { 'id':xx, 'file':xx, 'url':xx, 'type':xx }
+    def media_upload (self, source, name, mime = None):
+        if isinstance(source, str):
+            content = open(source, 'rb').read()
+        if hasattr(source, 'read'):
+            content = source.read()
+        else:
+            content = source
+        if mime is None:
+            if content[:3] == b'\xff\xd8\xff':
+                mime = 'image/jpeg'
+            elif content[:5] == b'\x89PNG\x0d':
+                mime = 'image/png'
+            elif content[:2] == b'\x42\x4d':
+                mime = 'image/x-bmp'
+            elif content[:5] == b'GIF89':
+                mime = 'image/gif'
+            elif content[:2] == b'\x50\x4b':
+                mime = 'application/zip'
+            elif content[:3] == b'\x37\x7a\xbc':
+                mime = 'application/x-7z-compressed'
+        data = {}
+        data['name'] = name
+        data['bits'] = wordpress_xmlrpc.compat.xmlrpc_client.Binary(content)
+        if mime:
+            data['type'] = mime
+        action = wordpress_xmlrpc.methods.media.UploadFile(data)
+        response = self._client.call(action)
+        return response
+
+    def media_get (self, attachment_id):
+        action = wordpress_xmlrpc.methods.media.GetMediaItem(attachment_id)
+        return self._client.call(action)
+
+    # keys of query: number, offset, parent_id, mime_type
+    # returns list of WordPressMedia instances
+    def media_list (self, query):
+        action = wordpress_xmlrpc.methods.media.GetMediaLibrary(query)
+        return self._client.call(action)
+
+
+
 
 
 #----------------------------------------------------------------------
@@ -94,9 +148,34 @@ if __name__ == '__main__':
         return 0
     def test2():
         wp = WordPress('localhost/web/blog', 'skywind', '678900')
-        print(wp.new_post())
+        pid = wp.post_new()
+        post = {}
+        import ascmini
+        post['id'] = pid
+        post['content'] = 'Now is: ' + ascmini.timestamp()
+        post['title'] = 'robot'
+        post['status'] = 'publish'
+        post['category'] = ['life']
+        post['tag'] = ['ai', 'game']
+        print(wp.post_edit(post))
         return 0
-    test2()
+    def test3():
+        wp = WordPress('localhost/web/blog', 'skywind', '678900')
+        post = wp.post_get(40)
+        print(post)
+        print(post.link)
+        print(post.content)
+        print(post.id)
+        return 0
+    def test4():
+        wp = WordPress('localhost/web/blog', 'skywind', '678900')
+        query = {}
+        query['offset'] = 0
+        for post in wp.post_list(query):
+            print(post)
+            print(post.link)
+        return 0
+    test4()
 
 
 
